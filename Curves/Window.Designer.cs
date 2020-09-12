8ff9f647c4c09b7bb6281eba8202dc535206c3de
Filipe Ramos <ik.ben.filipe@gmail.com>
2020-09-12 15:16:24 -0300

    Buttons and checkbox

--- Curves/Window.Designer.cs
+++ Curves/Window.Designer.cs
@@ -33,6 +33,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.chBox = new System.Windows.Forms.CheckBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -52,6 +55,9 @@
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.chBox);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel2.Location = new System.Drawing.Point(0, 417);
             this.panel2.Name = "panel2";
@@ -85,6 +91,34 @@
             this.button3.Text = "C1";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(245, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "G2";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(325, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "C2";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // chBox
+            // 
+            this.chBox.Name = "chBox";
+            this.chBox.Location = new System.Drawing.Point(410, 6);
+            this.chBox.Text = "Show control points";
+            this.chBox.Size = new System.Drawing.Size(120, 23);
+            this.chBox.Checked = true;
+            this.chBox.TabIndex = 5;
+            this.chBox.UseVisualStyleBackColor = true;
+            // 
             // Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -105,7 +139,8 @@
 
         private DrawPanel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button1, button2, button3;
+        private System.Windows.Forms.Button button1, button2, button3, button4, button5;
+        private System.Windows.Forms.CheckBox chBox;
     }
 }
 
